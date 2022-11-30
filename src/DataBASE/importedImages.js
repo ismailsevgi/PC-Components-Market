de76a2b4881b4fd1fbad425a9a06240e842c1d50
ismail sevgi <ismailsevgi95@gmail.com>
2022-11-30 04:15:22 +0300

    .

--- src/DataBASE/importedImages.js
+++ src/DataBASE/importedImages.js
@@ -5,7 +5,7 @@ import transRam from '../Images/colourful/colorTrans/ramColor-removebg-preview.p
 import transSsd from '../Images/colourful/colorTrans/ssdColor-removebg-preview.png';
 import transHdd from '../Images/colourful/colorTrans/hddColor-removebg-preview.png';
 import transCase from '../Images/colourful/colorTrans/caseColor-removebg-preview.png';
-import transPsu from '../Images/colourful/colorTrans/PsuColor-removebg-preview.png';
+import transPsu from '../Images/colourful/colorTrans/psuColor-removebg-preview.png';
 import arrowColor from '../Images/colourful/colorTrans/arrowColor-removebg-preview.png';
 import hearthColor from '../Images/colourful/colorTrans/hearthColor-removebg-preview.png';
 
