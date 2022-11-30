b587e96dc334bdefe8d5e663b4ff651ea24b9a08
ismail sevgi <ismailsevgi95@gmail.com>
2022-11-30 04:13:53 +0300

    changes: cleaning up unnecessary files; unused images

--- src/DataBASE/importedImages.js
+++ src/DataBASE/importedImages.js
@@ -2,9 +2,9 @@ import transGpu from '../Images/colourful/colorTrans/gpuColor-removebg-preview.p
 import transCpu from '../Images/colourful/colorTrans/cpuColor-removebg-preview.png';
 import transMobo from '../Images/colourful/colorTrans/moboColor-removebg-preview.png';
 import transRam from '../Images/colourful/colorTrans/ramColor-removebg-preview.png';
-import transSsd from '../Images/colourful/colorTrans/SsdColor-removebg-preview.png';
-import transHdd from '../Images/colourful/colorTrans/HddColor-removebg-preview.png';
-import transCase from '../Images/colourful/colorTrans/CaseColor-removebg-preview.png';
+import transSsd from '../Images/colourful/colorTrans/ssdColor-removebg-preview.png';
+import transHdd from '../Images/colourful/colorTrans/hddColor-removebg-preview.png';
+import transCase from '../Images/colourful/colorTrans/caseColor-removebg-preview.png';
 import transPsu from '../Images/colourful/colorTrans/PsuColor-removebg-preview.png';
 import arrowColor from '../Images/colourful/colorTrans/arrowColor-removebg-preview.png';
 import hearthColor from '../Images/colourful/colorTrans/hearthColor-removebg-preview.png';
